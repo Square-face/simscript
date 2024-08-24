d3f357b5ecf6a3f2f3d18fa9fbea909739828463
Square-face <linus.michelsson@gmail.com>
2024-08-24 12:55:30 +0200

    Added cube to scene

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use bevy::{
     app::{App, Startup},
+    asset::AssetServer,
     core_pipeline::core_3d::Camera3dBundle,
-    ecs::{component::Component, system::Commands},
+    ecs::{
+        component::Component,
+        system::{Commands, Res},
+    },
     math::Vec3,
+    scene::SceneBundle,
     transform::components::Transform,
     utils::default,
     DefaultPlugins,
@@ -14,7 +19,7 @@ struct PrimaryCameraMarker;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (setup_camera,))
+        .add_systems(Startup, (setup_camera, spawn_cube))
         .run();
 }
 
@@ -26,4 +31,12 @@ fn setup_camera(mut commands: Commands) {
         },
         PrimaryCameraMarker,
     ));
+}
+
+fn spawn_cube(mut commands: Commands, ass: Res<AssetServer>) {
+    let cube = ass.load("cube.glb#Scene0");
+    commands.spawn(SceneBundle {
+        scene: cube,
+        ..default()
+    });
 }
