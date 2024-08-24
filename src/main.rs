34c8be5f6f3fd540e9017fba9e887dffdbf1e198
Square-face <linus.michelsson@gmail.com>
2024-08-24 12:36:36 +0200

    Added Primary Camera

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
-use bevy::{app::App, DefaultPlugins};
+use bevy::{
+    app::{App, Startup},
+    core_pipeline::core_3d::Camera3dBundle,
+    ecs::{component::Component, system::Commands},
+    math::Vec3,
+    transform::components::Transform,
+    utils::default,
+    DefaultPlugins,
+};
+
+#[derive(Component)]
+struct PrimaryCameraMarker;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup_camera,))
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(10.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        PrimaryCameraMarker,
+    ));
 }
