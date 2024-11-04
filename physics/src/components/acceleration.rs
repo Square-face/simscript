331120a2fa1aa7a2d589809efe9ed380a70f6094
Square-face <sq8.square.face@gmail.com>
2024-11-04 09:10:17 +0100

    feat: can now mul/div Acc,AngAcc with f32

--- physics/src/components/acceleration.rs
+++ physics/src/components/acceleration.rs
@@ -4,12 +4,23 @@ use std::ops;
 
 use bevy::{ecs::component::Component, math::Vec3};
 
+use super::velocity::{AngularVelocity, Velocity};
+
 /// Represents a linear acceleration
 ///
-/// Works similar to [Velocity] in that the acceleration is represented as a Vec3 in global
-/// cordinates
+/// Ment to be used together with [Velocity]
+///
+/// Values are in `m/s^2`
 #[derive(Component, Debug, PartialEq)]
 pub struct Acceleration(pub Vec3);
+
+/// Represents a rotational acceleration
+///
+/// Meant to be used together with [AngularVelocity]
+///
+/// Values are in `rad/s^2`
+#[derive(Component, Debug, PartialEq)]
+pub struct AngularAcceleration(pub Vec3);
 
 impl Acceleration {
     /// [Accelerator] that doesn't accelerate in any direction
@@ -17,6 +28,11 @@ impl Acceleration {
 
     /// [Accelerator] that mimics gravity (-9.82 m/s^2 in y velocity)
     pub const GRAVITY: Self = Self(Vec3::new(0.0, -9.82, 0.0));
+}
+
+impl AngularAcceleration {
+    /// [Accelerator] that doesn't accelerate in any direction
+    pub const ZERO: Self = Self(Vec3::ZERO);
 }
 
 // Other
@@ -29,6 +45,10 @@ impl PartialOrd for Acceleration {
         this.partial_cmp(&other)
     }
 }
+overload!(- (a: ?Acceleration) -> Acceleration { Acceleration ( -a.0 )});
+overload!(- (a: ?AngularAcceleration) -> AngularAcceleration { AngularAcceleration ( -a.0 )});
+
+// Acceleration, Acceleration
 
 overload!((a: ?Acceleration) + (b: ?Acceleration) -> Acceleration { Acceleration ( a.0 + b.0 )});
 overload!((a: ?Acceleration) - (b: ?Acceleration) -> Acceleration { Acceleration ( a.0 - b.0 )});
@@ -40,4 +60,27 @@ overload!((a: &mut Acceleration) -= (b: ?Acceleration) { a.0 -= b.0; });
 overload!((a: &mut Acceleration) *= (b: ?Acceleration) { a.0 *= b.0; });
 overload!((a: &mut Acceleration) /= (b: ?Acceleration) { a.0 /= b.0; });
 
-overload!(- (a: ?Acceleration) -> Acceleration { Acceleration ( -a.0 )});
+// Acceleration, f32
+
+overload!((a: ?Acceleration) * (b: f32) -> Velocity { Velocity ( a.0 * b )});
+overload!((a: ?Acceleration) / (b: f32) -> Velocity { Velocity ( a.0 / b )});
+
+overload!((a: &mut Acceleration) *= (b: f32) { a.0 *= b; });
+overload!((a: &mut Acceleration) /= (b: f32) { a.0 /= b; });
+
+// Angular Acceleration, Angular Acceleration
+
+overload!((a: ?AngularAcceleration) + (b: ?AngularAcceleration) -> AngularAcceleration { AngularAcceleration ( a.0 + b.0 )});
+overload!((a: ?AngularAcceleration) - (b: ?AngularAcceleration) -> AngularAcceleration { AngularAcceleration ( a.0 - b.0 )});
+overload!((a: ?AngularAcceleration) * (b: ?AngularAcceleration) -> AngularAcceleration { AngularAcceleration ( a.0 * b.0 )});
+overload!((a: ?AngularAcceleration) / (b: ?AngularAcceleration) -> AngularAcceleration { AngularAcceleration ( a.0 / b.0 )});
+
+overload!((a: &mut AngularAcceleration) += (b: ?AngularAcceleration) { a.0 += b.0; });
+overload!((a: &mut AngularAcceleration) -= (b: ?AngularAcceleration) { a.0 -= b.0; });
+overload!((a: &mut AngularAcceleration) *= (b: ?AngularAcceleration) { a.0 *= b.0; });
+overload!((a: &mut AngularAcceleration) /= (b: ?AngularAcceleration) { a.0 /= b.0; });
+
+// AngularAcceleration, f32
+
+overload!((a: ?AngularAcceleration) * (b: f32) -> AngularVelocity { AngularVelocity ( a.0 * b )});
+overload!((a: ?AngularAcceleration) / (b: f32) -> AngularVelocity { AngularVelocity ( a.0 / b )});
